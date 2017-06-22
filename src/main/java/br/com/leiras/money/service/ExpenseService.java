c7053515d8f9c0633cc7ea83631086a627adef81
leiras <leiras@192.168.25.208>
2017-06-22 18:54:34 -0300

    Melhoria no serviço de inclusão de despesas.

--- src/main/java/br/com/leiras/money/service/ExpenseService.java
+++ src/main/java/br/com/leiras/money/service/ExpenseService.java
@@ -12,15 +12,17 @@ import br.com.leiras.money.model.Expense;
 public class ExpenseService {
 	
 	private Map<Integer, Expense> expensesMap = null;
+	private Integer expenseIndex = null;
 	
 	public ExpenseService() {
-		this.expensesMap = new HashMap<Integer, Expense>();				
+		this.expensesMap = new HashMap<Integer, Expense>();	
+		this.expenseIndex = 0;
 		try {
 			List<Category> categories = ServiceFactory.getCategoryService().listAllCategories();
 			
 			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 			Expense d1 = new Expense();
-	    	d1.setId(12);
+	    	d1.setId(++this.expenseIndex);
 	        d1.setValue(900f);  
 	        d1.setDate(formatter.parse("02/06/2017"));
 	        d1.setCategory(categories.get(0));
@@ -28,14 +30,14 @@ public class ExpenseService {
 	        this.expensesMap.put(d1.getId(), d1);
 	        
 	    	Expense d2 = new Expense();
-	    	d2.setId(15);
+	    	d2.setId(++this.expenseIndex);
 	        d2.setValue(329f);   
 	        d2.setDate(formatter.parse("15/06/2017"));
 	        d2.setCategory(categories.get(2));
 	        this.expensesMap.put(d2.getId(), d2);  
 	        
 	        Expense d3 = new Expense();
-	    	d3.setId(19);
+	    	d3.setId(++this.expenseIndex);
 	        d3.setValue(590f);   
 	        d3.setDate(formatter.parse("20/06/2017"));
 	        d3.setCategory(categories.get(3));
@@ -54,20 +56,18 @@ public class ExpenseService {
 		return this.expensesMap.get(expenseId);
 	}
 	
-	public Expense addExpense(Expense expense) throws Exception {
-		System.out.println("Expense "+expense.getId()+" added!");
-		this.expensesMap.put(expense.getId(), expense);
+	public Expense addExpense(Expense expense) throws Exception {					
+		this.expensesMap.put(++this.expenseIndex, expense);
+		expense.setId(this.expenseIndex);	
 		return expense;
 	}
 	
-	public Expense updateExpense(Expense expense) throws Exception {
-		System.out.println("Expense "+expense.getId()+" updated!");
+	public Expense updateExpense(Expense expense) throws Exception {		
 		this.expensesMap.put(expense.getId(), expense);
 		return expense;
 	}	
 	
-	public void deleteExpense(Integer expenseId) throws Exception {
-		System.out.println("Expense "+expenseId+" deleted!");
+	public void deleteExpense(Integer expenseId) throws Exception {		
 		this.expensesMap.remove(expenseId);
 	}
 	
